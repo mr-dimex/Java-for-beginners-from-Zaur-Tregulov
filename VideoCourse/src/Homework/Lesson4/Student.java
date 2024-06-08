f5447afd721f31f9ce11907907aaf4797ad1d435
Dmitrii Belikhin <152195107+mr-dimex@users.noreply.github.com>
2024-06-09 00:54:28 +0300

    Update Student.java
    Update

--- VideoCourse/src/Homework/Lesson4/Student.java
+++ VideoCourse/src/Homework/Lesson4/Student.java
@@ -13,7 +13,7 @@ public class Student {
 class StudentTest {
     public static void main (String[] args) {
 
-        Student st1 = new Student();
+        Student st1 = new Student();        // Создание первого объекта
         st1.studentID = 1;
         st1.studentName = "Dmitrii";
         st1.studentSurname = "Belikhin";
@@ -22,7 +22,7 @@ class StudentTest {
         st1.averageRatingEconomics = 4.0;
         st1.averageRatingForeignLanguage = 5.0;
 
-        Student st2 = new Student();
+        Student st2 = new Student();        // Создание второго объекта
         st2.studentID = 2;
         st2.studentName = "Anna";
         st2.studentSurname = "Petrova";
@@ -31,7 +31,7 @@ class StudentTest {
         st2.averageRatingEconomics = 4.7;
         st2.averageRatingForeignLanguage = 4.4;
 
-        Student st3 = new Student();
+        Student st3 = new Student();        // Создание третьего объекта
         st3.studentID = 3;
         st3.studentName = "Aleksey";
         st3.studentSurname = "Ivanov";
@@ -40,6 +40,7 @@ class StudentTest {
         st3.averageRatingEconomics = 4.0;
         st3.averageRatingForeignLanguage = 4.2;
 
+        // Расчёт средних арифметических оценок студентов
         System.out.println("Средняя арифметическая оценка студента " + st1.studentName + " " + st1.studentSurname + " равна: "
                 + (st1.averageRatingMath + st1.averageRatingEconomics + st1.averageRatingForeignLanguage) / 3);
         System.out.println("Средняя арифметическая оценка студента " + st2.studentName + " " + st2.studentSurname + " равна: "
